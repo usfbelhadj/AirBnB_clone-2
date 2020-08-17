8126867f948ccc8b5039bc5e6fd367295baeb5bb
usfbelhadj <usf.belhadj@gmail.com>
2020-08-17 19:52:17 +0100

    Test

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+ #!/usr/bin/env bash
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu: /data/
+sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
