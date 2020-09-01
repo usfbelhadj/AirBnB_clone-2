37e1765e95f318a931ffcd83ce1df091d290d012
usfbelhadj <usf.belhadj@gmail.com>
2020-09-01 13:52:22 +0100

    Test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Hello Route'''
+
 from flask import Flask
+
 
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 '''Hbnb Route'''
+
 from flask import Flask
+
 
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def helloHbnb():
