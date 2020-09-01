218afdce2420ea3a6ebd4bc4ff88191bf69aace3
usfbelhadj <usf.belhadj@gmail.com>
2020-09-01 13:34:57 +0100

    hvt

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def helloHbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    return 'C {}'.format(text)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
