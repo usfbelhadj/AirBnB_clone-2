dd35f1617bb967132508379d3a564438d36808fa
usfbelhadj <usf.belhadj@gmail.com>
2020-09-01 12:20:45 +0100

    HEllo_Route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
